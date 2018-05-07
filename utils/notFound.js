938cc66eeafa104c3aaf2950ba558f22027806e8
luke <625376618@qq.com>
2018-05-07 17:05:55 +0800

    Create notFound.js

---
+++ utils/notFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class NotFound extends React.Component{
+    render(){
+        return(
+            <div style={{textAlign:'center',marginTop:'20px',fontSize:"30px"}}>
+              Opps,该页面不可见
+            </div>
+        )
+    }
+}
+
+
+export default NotFound
